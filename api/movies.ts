5e6f28f2cd94e551dea1a1a98792e95a155605c3
IRSAGE <sibo.egide98@gmail.com>
2024-05-02 22:57:31 +0200

    Feching Movies Details

--- api/movies.ts
+++ api/movies.ts
@@ -1,16 +1,17 @@
 const apiKey =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTg3ZmM4MTc5YzkwYmU4ZjUwMjUzNGNmMjEzMjk4NyIsInN1YiI6IjYyYzJkYTZhNmEzMDBiMDA1OTllNWYxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UeQMfkFglpe0rGDn2CAyk3ZtGrEF593zs6oXxj7eNEg";
 
-export const fetchTopRatedMovies = async () => {
-  const url =
-    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+const url =
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
 
+const headers = {
+  accept: "application/json",
+  Authorization: "Bearer " + apiKey,
+};
+export const fetchTopRatedMovies = async () => {
   const options = {
     method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: "Bearer " + apiKey,
-    },
+    headers,
   };
   const res = await fetch(url, options);
 
@@ -20,4 +21,21 @@ export const fetchTopRatedMovies = async () => {
 
   const json = await res.json();
   return json.results;
+};
+
+export const fetchMovie = async (id: number) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: "GET",
+    headers,
+  };
+
+  const res = await fetch(url, options);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch movies");
+  }
+
+  const json = await res.json();
+  return json;
 };
